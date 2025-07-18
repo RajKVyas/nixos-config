62bc0cb0da3ccca077ab7e64816feaed6363a39a
Raj Kumar Vyas <inbox@rajvyas.com>
2025-07-17 20:40:11 -0400

    minor changes

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -9,6 +9,43 @@
     ../../modules/nixos/gnome.nix
     # We will add more here later, like hyprland.nix, nvidia.nix, etc.
   ];
+
+  fileSystems = {
+    "/mnt/projects" = {
+      device = "/dev/disk/by-uuid/5AB84472B8444F27";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000" "gid=100" "nofail" ];
+    };
+
+    "/mnt/media" = {
+      device = "/dev/disk/by-uuid/78AC445CAC4416D2";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000" "gid=100" "nofail" ];
+    };
+
+    "/mnt/backup" = {
+      device = "/dev/disk/by-uuid/763C181F3C17D947";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000" "gid=100" "nofail" ];
+    };
+    
+    "/mnt/data" = {
+      device = "/dev/disk/by-uuid/FE4A4B8A4A4B3EA1";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000" "gid=100" "nofail" ];
+    };
+
+    "/mnt/main" = {
+      device = "/dev/disk/by-uuid/568C8DA38C8D7DED";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000" "gid=100" "nofail" ];
+    };
+    "/mnt/windows" = {
+      device = "/dev/disk/by-uuid/686832096831D712";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000" "gid=100" "nofail" ];
+    };
+  };
   
   users.users.raj = {
     isNormalUser = true;
